1d82bf3639e89fb7d66ffa7ba4809ea50090ce9a
SENX | センクス <senseixdev@gmail.com>
2025-10-29 08:25:20 +0000

    munch: Initialize for @AxionAOSP 2.x

--- lineage_munch.mk
+++ lineage_munch.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2024 The LineageOS Project
+# Copyright (C) 2021 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -14,17 +14,35 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from munch device
 $(call inherit-product, device/xiaomi/munch/device.mk)
 
-# Boot animation resolution
-TARGET_BOOT_ANIMATION_RES := 1080
-
 PRODUCT_NAME := lineage_munch
 PRODUCT_DEVICE := munch
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := 22021211RC
+PRODUCT_BRAND := Poco
+PRODUCT_MODEL := 22021211RG
+
+# EPPE
+TARGET_DISABLE_EPPE := true
+
+# BLUR
+TARGET_ENABLE_BLUR := true
+
+# AXION
+AXION_CAMERA_REAR_INFO := 64,8,2
+AXION_CAMERA_FRONT_INFO := 20
+AXION_MAINTAINER := SENX|センクス
+AXION_PROCESSOR := Snapdragon®_870_5G
+PERF_GOV_SUPPORTED := true
+PERF_DEFAULT_GOV := schedutil
+PERF_ANIM_OVERRIDE := true
+
+# LOS PREBUILTS
+TARGET_INCLUDES_LOS_PREBUILTS := true
+
+# BYPASS CHARGING
+BYPASS_CHARGE_SUPPORTED := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="munch-user 13 RKQ1.211001.001 V816.0.9.0.ULMMIXM release-keys" \
-    BuildFingerprint=Redmi/munch/munch:13/RKQ1.211001.001/V816.0.9.0.ULMMIXM:user/release-keys
+    BuildFingerprint=POCO/munch_global/munch:13/RKQ1.211001.001/V816.0.9.0.ULMMIXM:user/release-keys
